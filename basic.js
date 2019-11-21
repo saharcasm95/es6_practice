2eb16aea1285c1e88ff005b8515fae64621030b3
Saharcasm <nooresahar95@gmail.com>
2019-11-21 17:11:02 +1100

    new methods added

--- basic.js
+++ basic.js
@@ -5,5 +5,14 @@
     function getRandomNumber(){
         return Math.random()
     }
+    function getWebsiteUrl(){
+        console.log(window.location.href);
+        return window.location.href;
+    }
+    function createVariable(global, varName){
+        global[varName] = "";
+    }
+
+
 })();
 
--- strings.js
+++ strings.js
@@ -7,6 +7,16 @@
         testString = lastChar + testString.substring(0, lastIndex);
     }
 
-    rotateString();
+    function getFileExtension(file) {
+        let testing = file.split('.').pop();
+        console.log(testing);
+    }
+
+    function checkPrefix(prefix, testString){
+        let prefixPattern = new RegExp(`^${prefix}`);
+        let prefixMatch = testString.match(prefixPattern);
+        return  prefixMatch ? testString : prefix+testString;
+    }
+    console.log(checkPrefix("py", "testString"));
 
 })();
